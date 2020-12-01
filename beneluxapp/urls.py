c18fd7b3a10c086094d1ac99d7d28bf91f40512a
saifulshihab <shihabmd1970@gmail.com>
2020-12-01 19:21:05 +0600

    add new static template

--- beneluxapp/urls.py
+++ beneluxapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('diensten/', views.diensten, name='diensten'),
+    path('project/', views.project, name='project'),
+    path('prijscalculator/', views.prijscalculator, name='prijscalculator'),
+    path('contact/', views.contact, name='contact'),
 ]
