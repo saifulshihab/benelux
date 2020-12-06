dd4b0e1c3430e72c3e181a6ec78dedbd48033c06
saifulshihab <shihabmd1970@gmail.com>
2020-12-06 16:20:42 +0600

    project item upload from administration

--- beneluxapp/models.py
+++ beneluxapp/models.py
@@ -5,6 +5,7 @@ from PIL import Image
 class projects(models.Model):
     p_title = models.CharField(max_length=150, blank=False)
     p_image = models.ImageField(default='default-image.png', upload_to='projects')
+    location = models.CharField(max_length=100, blank=False)
 
     def save(self, *args, **kwargs):
         super(projects, self).save(*args, **kwargs)
@@ -12,6 +13,6 @@ class projects(models.Model):
         img = Image.open(self.p_image.path)
 
         if img.height > 300 or img.width >300:
-            output_size =(300, 300)
+            output_size =(img.height, img.width)
             img.thumbnail(output_size)
             img.save(self.p_image.path)
