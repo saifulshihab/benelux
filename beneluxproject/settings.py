e47d7266be388a670c81c412aeddfd1c687e2db9
abdishukri <abdi.shakib@hendrikx-itc.nl>
2020-12-18 08:42:39 +0100

    mail

--- beneluxproject/settings.py
+++ beneluxproject/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 # Email Config
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'mail.jouwweb.nl'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'info@beneluxdakkapellen.nl'
